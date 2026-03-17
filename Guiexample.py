92b9326dc8c4c858e839b04109382fa1913064a6
alifhaikal-boop <aliffzafran2003@gmail.com>
2026-03-17 11:03:19 +0800

    Delete Guiexample.py

--- Guiexample.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Demo Streamlit")
-st.write("Hello! Welcome to Beginner Website Using Streamlit")
-
-name = st.text_input("Your Name:")
-if st.button("Submit"):
-   st.success(f"Hai {name}! Welcome to this Beginner Website")
